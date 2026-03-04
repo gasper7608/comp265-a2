17c9db6470b85a6719fa46a06e2ef2d2b7546e6a
gasper7608 <gasper7608@saskpolytech.ca>
2026-03-03 18:55:35 -0600

    added favourites tab to make it look like home page but only show favs using data

--- assignment2/app/(tabs)/favourites/index.tsx
+++ assignment2/app/(tabs)/favourites/index.tsx
@@ -1,20 +1,64 @@
-import { View, Text, StyleSheet } from "react-native";
+import { ScrollView, Text, StyleSheet, Image } from "react-native";
+import { Link } from "expo-router";
+import { recipes } from "../../data/recipes";
 
-export default function Favourites() {
+export default function FavouritesScreen() {
+    // only showing recipes that show isFavorite = true
+    const favouriteRecipes = recipes.filter((recipe) => recipe.isFavorite);
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Favourites Screen</Text>
-        </View>
+        <ScrollView style={styles.container}>
+            <Text style={styles.header}>Favourites</Text>
+
+            {/* using same style as home page */}
+            {favouriteRecipes.map((recipe) => (
+                <Link
+                    key={recipe.id}
+                    href={{
+                        pathname: "/home/details",
+                        params: {
+                            title: recipe.title,
+                            content: recipe.description,
+                        },
+                    }}
+                    style={styles.card}
+                >
+                    <Image source={{ uri: recipe.image }} style={styles.image} />
+                    <Text style={styles.title}>{recipe.title}</Text>
+                </Link>
+            ))}
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        backgroundColor: "#f4f6f8",
+        padding: 20,
     },
-    text: {
-        fontSize: 20,
+    header: {
+        fontSize: 28,
+        fontWeight: "bold",
+        marginBottom: 20,
+        marginTop: 40,
+    },
+    card: {
+        backgroundColor: "#d0d0d0",
+        padding: 18,
+        borderRadius: 12,
+        marginBottom: 20,
+        color: "black",
+        elevation: 3,
+    },
+    image: {
+        width: "100%",
+        height: 180,
+        borderRadius: 10,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: "600",
+        padding: 15,
     },
 });
